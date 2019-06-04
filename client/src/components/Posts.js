abaf2d6018c26a189b3e8698c76cf0308e3960aa
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-06-04 22:41:21 +0000

    cleaning

---
+++ client/src/components/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Posts extends React.Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
